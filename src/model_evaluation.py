a237ee56adb2527ffeaf964aac9699b708ac40bf
Haseeb <haseebasif5@gmail.com>
2025-01-14 12:10:12 +0500

    After running experiments

--- src/model_evaluation.py
+++ src/model_evaluation.py
@@ -6,6 +6,7 @@ import json
 from sklearn.metrics import accuracy_score, precision_score, recall_score, roc_auc_score
 import logging
 import yaml
+from dvclive import Live
 
 # Ensure the "logs" directory exists
 log_dir = 'logs'
@@ -78,7 +79,7 @@ def evaluate_model(clf, X_test: np.ndarray, y_test: np.ndarray) -> dict:
     try:
         y_pred = clf.predict(X_test)
         y_pred_proba = clf.predict_proba(X_test)[:, 1]
-
+        
         accuracy = accuracy_score(y_test, y_pred)
         precision = precision_score(y_test, y_pred)
         recall = recall_score(y_test, y_pred)
@@ -91,7 +92,7 @@ def evaluate_model(clf, X_test: np.ndarray, y_test: np.ndarray) -> dict:
             'auc': auc
         }
         logger.debug('Model evaluation metrics calculated')
-        return metrics_dict
+        return metrics_dict, y_pred
     except Exception as e:
         logger.error('Error during model evaluation: %s', e)
         raise
@@ -118,8 +119,15 @@ def main():
         X_test = test_data.iloc[:, :-1].values
         y_test = test_data.iloc[:, -1].values
 
-        metrics = evaluate_model(clf, X_test, y_test)
+        metrics,y_pred = evaluate_model(clf, X_test, y_test)
         
+        with Live(save_dvc_exp=True) as live:
+            live.log_metric('accuracy', accuracy_score(y_test, y_pred))
+            live.log_metric('precision', precision_score(y_test, y_pred))
+            live.log_metric('recall', recall_score(y_test, y_pred))
+
+            live.log_params(params)
+            
         save_metrics(metrics, 'reports/metrics.json')
     except Exception as e:
         logger.error('Failed to complete the model evaluation process: %s', e)
